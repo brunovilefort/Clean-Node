e20b2f40a98208c51ee063b641a50b1c7d123cac
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-29 15:09:45 -0300

    chore: add git-commit-linter package

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**',
-    '!<rootDir>/src/**/index.ts'
+    '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   moduleNameMapper: {
     '@/tests/(.+)': '<rootDir>/tests/$1',
     '@/(.+)': '<rootDir>/src/$1'
