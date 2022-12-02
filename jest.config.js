0abac87a880d5a20c36fa1726947bb21ce6d536f
Bruno Vilefort <bruno.clara@yahoo.com>
2022-12-02 19:38:17 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   moduleNameMapper: {
     '@/tests/(.+)': '<rootDir>/tests/$1',
     '@/(.+)': '<rootDir>/src/$1'
