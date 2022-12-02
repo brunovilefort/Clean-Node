88b4588900c6f1e2e755641f1a00d8716fd12a8f
Bruno Vilefort <bruno.clara@yahoo.com>
2022-12-02 14:43:11 -0300

    chore: add express, sucrase,dotenv and module-alias packages

---
+++ src/main/index.ts
@@ -0,0 +1,8 @@
+import './config/module-alias'
+import { app } from './app'
+
+import 'dotenv/config'
+
+const port = process.env.API_PORT
+
+app.listen(port, () => console.log(`🏃 Server is running on http://localhost:${port}`))
